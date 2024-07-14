ce35bbf0169dfe57795333cda05fc765dcfad639
Nils Diekmann <github@kinneko.de>
2024-07-14 15:28:21 +0200

    - prettier

--- src/main.js
+++ src/main.js
@@ -1,47 +1,53 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+const core = require('@actions/core')
+const github = require('@actions/github')
 
 async function run() {
   try {
-    const token = core.getInput('token', { required: true });
-    const octokit = github.getOctokit(token);
+    const token = core.getInput('token', { required: true })
+    const octokit = github.getOctokit(token)
 
-    const existingFeatureBranches = (await octokit.rest.repos.listBranches({
-      owner: github.context.repo.owner,
-      repo: github.context.repo.repo,
-    })).data.map(branch => branch.name);
+    const existingFeatureBranches = (
+      await octokit.rest.repos.listBranches({
+        owner: github.context.repo.owner,
+        repo: github.context.repo.repo
+      })
+    ).data.map(branch => branch.name)
 
-    console.log("Existing feature branches:", existingFeatureBranches);
+    console.log('Existing feature branches:', existingFeatureBranches)
 
     const tags = await octokit.rest.git.listMatchingRefs({
       owner: github.context.repo.owner,
       repo: github.context.repo.repo,
-      ref: 'tags/v',
-    });
+      ref: 'tags/v'
+    })
 
     for (const tag of tags.data) {
-      const tagName = tag.ref.replace('refs/tags/', '');
-      core.debug("Tag: " + tagName);
+      const tagName = tag.ref.replace('refs/tags/', '')
+      core.debug('Tag: ' + tagName)
 
-      const tagParts = /^v[0-9]*\.[0-9]*\.[0-9]*-(.*)\.([0-9]*)$/.exec(tagName);
+      const tagParts = /^v[0-9]*\.[0-9]*\.[0-9]*-(.*)\.([0-9]*)$/.exec(tagName)
       if (tagParts) {
-        const featureBranchName = tagParts[1];
-        core.debug("Feature branch name: " + featureBranchName);
+        const featureBranchName = tagParts[1]
+        core.debug('Feature branch name: ' + featureBranchName)
 
         if (!existingFeatureBranches.includes(`feature/${featureBranchName}`)) {
-          core.log(`Branch ${featureBranchName} does not exist, so deleting tag ${tagName}`);
+          core.log(
+            `Branch ${featureBranchName} does not exist, so deleting tag ${tagName}`
+          )
           await octokit.rest.git.deleteRef({
             owner: github.context.repo.owner,
             repo: github.context.repo.repo,
-            ref: `tags/${tagName}`,
-          });
+            ref: `tags/${tagName}`
+          })
         } else {
-          core.debug(`Branch ${featureBranchName} exists, so not deleting tag ${tagName}`);
+          core.debug(
+            `Branch ${featureBranchName} exists, so not deleting tag ${tagName}`
+          )
         }
       }
     }
   } catch (error) {
-    core.setFailed(error.message);
+    core.setFailed(error.message)
   }
 }
 
